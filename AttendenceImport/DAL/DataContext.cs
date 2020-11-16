5ed2485392a9dd13273301d69c7bf74675c5665e
Md. Abul Kalam <akazad.cse13@gmail.com>
2020-11-16 23:58:11 +0600

    Initial structure

---
+++ AttendenceImport/DAL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using AttendenceImport.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AttendenceImport.DAL
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> opts)
+            : base(opts) { }
+
+        public DbSet<ExcelData> ExcelData { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ExcelData>().HasKey(ed => new
+            {
+                ed.StudentID,
+                ed.ProgrammeID
+            });
+        }
+    }
+}
---
+++ AttendenceImport/Models/ExcelData.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AttendenceImport.Models
+{
+    public class ExcelData
+    {
+        public string StudentName { get; set; }
+        public int StudentID { get; set; }
+        public int ProgrammeID { get; set; }
+        [MaxLength(50)]
+        public string ClassCode { get; set; }
+        [MaxLength(50)]
+        public string StudentStatus { get; set; }
+        public string AttendanceStatus { get; set; }
+        public char? L1 { get; set; }
+        [MaxLength(50)]
+        public string Attendance1 { get; set; }
+        public char? L2 { get; set; }
+        [MaxLength(50)]
+        public string Attendance2 { get; set; }
+        public char? L3 { get; set; }
+        [MaxLength(50)]
+        public string Attendance3 { get; set; }
+        public char? L4 { get; set; }
+        [MaxLength(50)]
+        public string Attendance4 { get; set; }
+        public char? L5 { get; set; }
+        [MaxLength(50)]
+        public string Attendance5 { get; set; }
+        public char? L6 { get; set; }
+        [MaxLength(50)]
+        public string Attendance6 { get; set; }
+        public char? L7 { get; set; }
+        [MaxLength(50)]
+        public string Attendance7 { get; set; }
+        public char? L8 { get; set; }
+        [MaxLength(50)]
+        public string Attendance8 { get; set; }
+        public char? L9 { get; set; }
+        [MaxLength(50)]
+        public string Attendance9 { get; set; }
+        public char? L10 { get; set; }
+        [MaxLength(50)]
+        public string Attendance10 { get; set; }
+        public char? L11 { get; set; }
+        [MaxLength(50)]
+        public string Attendance11 { get; set; }
+        public char? L12 { get; set; }
+        [MaxLength(50)]
+        public string Attendance12 { get; set; }
+        public char? L13 { get; set; }
+        [MaxLength(50)]
+        public string Attendance13 { get; set; }
+        public char? L14 { get; set; }
+        [MaxLength(50)]
+        public string Attendance14 { get; set; }
+        public char? L15 { get; set; }
+        [MaxLength(50)]
+        public string Attendance15 { get; set; }
+        public char? L16 { get; set; }
+        [MaxLength(50)]
+        public string Attendance16 { get; set; }
+        public char? L17 { get; set; }
+        [MaxLength(50)]
+        public string Attendance17 { get; set; }
+        public char? L18 { get; set; }
+        [MaxLength(50)]
+        public string Attendance18 { get; set; }
+        public char? L19 { get; set; }
+        [MaxLength(50)]
+        public string Attendance19 { get; set; }
+        public char? L20 { get; set; }
+        [MaxLength(50)]
+        public string Attendance20 { get; set; }
+        public char? L21 { get; set; }
+        [MaxLength(50)]
+        public string Attendance21 { get; set; }
+        public char? L22 { get; set; }
+        [MaxLength(50)]
+        public string Attendance22 { get; set; }
+        public char? L23 { get; set; }
+        [MaxLength(50)]
+        public string Attendance23 { get; set; }
+        public char? L24 { get; set; }
+        [MaxLength(50)]
+        public string Attendance24 { get; set; }
+        public char? L25 { get; set; }
+        [MaxLength(50)]
+        public string Attendance25 { get; set; }
+        public char? L26 { get; set; }
+        [MaxLength(50)]
+        public string Attendance26 { get; set; }
+        public char? L27 { get; set; }
+        [MaxLength(50)]
+        public string Attendance27 { get; set; }
+        public char? L28 { get; set; }
+        [MaxLength(50)]
+        public string Attendance28 { get; set; }
+        public char? L29 { get; set; }
+        [MaxLength(50)]
+        public string Attendance29 { get; set; }
+        public char? L30 { get; set; }
+        [MaxLength(50)]
+        public string Attendance30 { get; set; }
+        public char? L31 { get; set; }
+        [MaxLength(50)]
+        public string Attendance31 { get; set; }
+        public char? L32 { get; set; }
+        [MaxLength(50)]
+        public string Attendance32 { get; set; }
+        public char? L33 { get; set; }
+        [MaxLength(50)]
+        public string Attendance33 { get; set; }
+        public char? L34 { get; set; }
+        [MaxLength(50)]
+        public string Attendance34 { get; set; }
+        public char? L35 { get; set; }
+        [MaxLength(50)]
+        public string Attendance35 { get; set; }
+        public char? L36 { get; set; }
+        [MaxLength(50)]
+        public string Attendance36 { get; set; }
+        public char? L37 { get; set; }
+        [MaxLength(50)]
+        public string Attendance37 { get; set; }
+        public char? L38 { get; set; }
+        [MaxLength(50)]
+        public string Attendance38 { get; set; }
+        public char? L39 { get; set; }
+        [MaxLength(50)]
+        public string Attendance39 { get; set; }
+        public char? L40 { get; set; }
+        [MaxLength(50)]
+        public string Attendance40 { get; set; }
+        public char? L41 { get; set; }
+        [MaxLength(50)]
+        public string Attendance41 { get; set; }
+        public char? L42 { get; set; }
+        [MaxLength(50)]
+        public string Attendance42 { get; set; }
+        public char? L43 { get; set; }
+        [MaxLength(50)]
+        public string Attendance43 { get; set; }
+        public char? L44 { get; set; }
+        [MaxLength(50)]
+        public string Attendance44 { get; set; }
+        public char? L45 { get; set; }
+        [MaxLength(50)]
+        public string Attendance45 { get; set; }
+        public char? L46 { get; set; }
+        [MaxLength(50)]
+        public string Attendance46 { get; set; }
+        public char? L47 { get; set; }
+        [MaxLength(50)]
+        public string Attendance47 { get; set; }
+        public char? L48 { get; set; }
+        [MaxLength(50)]
+        public string Attendance48 { get; set; }
+        public char? L49 { get; set; }
+        [MaxLength(50)]
+        public string Attendance49 { get; set; }
+        public char? L50 { get; set; }
+        [MaxLength(50)]
+        public string Attendance50 { get; set; }
+        public char? L51 { get; set; }
+        [MaxLength(50)]
+        public string Attendance51 { get; set; }
+        public char? L52 { get; set; }
+        [MaxLength(50)]
+        public string Attendance52 { get; set; }
+        public char? L53 { get; set; }
+        [MaxLength(50)]
+        public string Attendance53 { get; set; }
+        public char? L54 { get; set; }
+        [MaxLength(50)]
+        public string Attendance54 { get; set; }
+        public char? L55 { get; set; }
+        [MaxLength(50)]
+        public string Attendance55 { get; set; }
+        public char? L56 { get; set; }
+        [MaxLength(50)]
+        public string Attendance56 { get; set; }
+    }
+}
